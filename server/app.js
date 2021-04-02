c9843f993d0036bdd69ab9e8ca0b1a022e37076e
unknown <medusa06@gmail.com>
2021-04-02 12:27:36 +0530

    updation

--- server/app.js
+++ server/app.js
@@ -29,7 +29,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public/index.html")));
+//app.use(express.static(path.join(__dirname, "public/index.html")));
+app.use(express.static("client/build"));
  //require("./middeleware/cros")(app);
 
 
@@ -63,6 +64,8 @@ app.use("/",TaskRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function (err, req, res, next) {
