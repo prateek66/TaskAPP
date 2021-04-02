74126b4f410c35327cf21671af09b0ef97a849f3
unknown <medusa06@gmail.com>
2021-04-02 15:04:21 +0530

    update

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public/index.html")));
+app.use(express.static(path.join(__dirname, "public")));
  //require("./middeleware/cros")(app);
 
 
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const path = require("path")
 const TaskContoller = require("../Controller/TaskController")
 
 const router = express.Router();
 
+console.log(path.join(__dirname, '..',"public", "index.html"));
 router.post('/create', TaskContoller.create);
 router.get('/view/:_id',TaskContoller.GetTask);
 router.get('/changeStatus/:_id',TaskContoller.changeStatus);
 router.get('/viewall',TaskContoller.getalltask);
+router.get('/',async(req,res)=> {
+    res.sendFile(path.join(__dirname, '..',"public", "index.html"));
+
+})
 
 module.exports = router;
